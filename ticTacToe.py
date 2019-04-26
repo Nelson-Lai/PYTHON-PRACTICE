f983ef54cc3bfaa598d7ea507d36e1d0f23219d1
Nelson-Lai <50011161+Nelson-Lai@users.noreply.github.com>
2019-04-26 01:52:15 -0700

    Update ticTacToe.py
    its finished

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,8 +1,19 @@
 '''Tic Tac Toe'''
-
+import random
 '''create a gameboard '''
 class TicTacToe:
+    def __init__(self):
+        self.newGameboard()
+        self.victory = 0
+        print("Let's play a game of TicTacToe")
+        self.gameLoop()
+
+
+
     boardLocations = {'topLeft':[0,1],'topMid':[0,5],'topRight':[0,9],'midLeft':[2,1],'midMid':[2,5],'midRight':[2,9],'botLeft':[4,1],'botMid':[4,5],'botRight':[4,9]}
+    backLocations = {'topLeft':[0,0],'topMid':[0,1],'topRight':[0,2],'midLeft':[1,0],'midMid':[1,1],'midRight':[1,2],'botLeft':[2,0],'botMid':[2,1],'botRight':[2,2]}
+
+
     def newGameboard(self):
         self.gameBoard = [
         '   |   |   ',
@@ -12,19 +23,96 @@ class TicTacToe:
         '   |   |   ',
         '   |   |   '
         ]
-        availableSet = set(['topLeft','topMid','topRight','midLeft','midMid','midRight','botLeft','botMid','botRight'])
+        self.availableSet = set(['topLeft','topMid','topRight','midLeft','midMid','midRight','botLeft','botMid','botRight'])
+        self.backBoard = [[0,0,0],[0,0,0],[0,0,0]]
 
     def placeMarker(self,location: str, mark: str):
-        x,y = boardLocations[location]
+        x,y = self.boardLocations[location]
+        xback, yback = self.backLocations[location]
+        if mark == 'x':
+            self.backBoard[xback][yback] = 1
+        elif mark == 'o':
+            self.backBoard[xback][yback] = -1
         boardRow = list(self.gameBoard[x])
         boardRow[y] = mark
         self.gameBoard[x] = ''.join(boardRow)
+        self.availableSet.remove(location)
 
     def displayBoard(self):
         for row in self.gameBoard:
             print(row)
 
+    def checkVictory(self) -> int:
+        for row in self.backBoard:
+            if sum(row) == 3:
+                return 1
+            elif sum(row) == -3:
+                return -1
+
+        for column in range(3):
+            columnsum = 0
+            for row in self.backBoard:
+                columnsum += row[column]
+            if columnsum == 3:
+                return 1
+            elif columnsum == -3:
+                return -1
+
+        diagonalsum1 = 0
+        diagonalsum2 = 0
+        for diagonal in range(2):
+            diagonalsum1 += self.backBoard[diagonal][diagonal]
+            diagonalsum2 += self.backBoard[diagonal][2-diagonal]
+
+        if diagonalsum1 == 3 or diagonalsum2 == 3:
+            return 1
+        elif diagonalsum2 == -3 or diagonalsum2 == -3:
+
+            return -1
+        return 0
+
+
+    def computerMove(self):
+        move = random.choice(list(self.availableSet))
+        self.placeMarker(move,'o')
+
+    def gameLoop(self):
+        while self.victory == 0:
+            if self.availableSet:
+                self.displayBoard()
+                self.victory = self.checkVictory()
+                if self.victory == 1:
+                    print('Player Victory')
+                    break
+                elif self.victory == -1:
+                    print('CPU Victory')
+                    break
+
+                print('Available Moves:')
+                print(self.availableSet)
+                move = input('>>')
+                if move not in self.availableSet:
+                    continue
+                self.placeMarker(move,'x')
+                self.displayBoard()
+
+                self.victory = self.checkVictory()
+                if self.victory == 1:
+                    print('Player Victory')
+                    break
+                elif self.victory == -1:
+                    print('CPU Victory')
+                    break
+
+                if self.availableSet:
+                    input('Press Enter to see Computer Move')
+                    self.computerMove()
+            else:
+                self.displayBoard()
+                print('Draw Game!')
+                break
+
+
+
+
 game = TicTacToe()
-game.newGameboard()
-game.placeMarker('topMid','x')
-game.displayBoard()
